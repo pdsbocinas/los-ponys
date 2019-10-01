e901004299df6123b663a25d92fb5e872086890b
77natas77 <javier.terranova@gmail.com>
2019-10-01 01:11:19 -0300

    borro acentos

--- src/main/webapp/js/host.js
+++ src/main/webapp/js/host.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export const host = "http://localhost:8081/Los_Ponys_war";
-=======
 export const host = "http://localhost:8080/Los_Ponys_war"
->>>>>>> 609473fbc5325414231e29a2ced2deeadd60b118
